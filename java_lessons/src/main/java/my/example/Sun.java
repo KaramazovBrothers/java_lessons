cd5b5297126d5b636eda657921b29b5b702d348f
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-01-30 00:37:23 +0100

    creatClassSun

---
+++ java_lessons/src/main/java/my/example/Sun.java
@@ -0,0 +1,12 @@
+package my.example;
+
+public class Sun {
+     public static void Rassvet() {
+     for (int i=0; i<366; i++) {
+         System.out.println("Рассвет№:" + " " + i);
+     }
+ }
+    public static void main(String[] args) {
+        Rassvet();
+    }
+}
