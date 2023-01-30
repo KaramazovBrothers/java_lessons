bfaf5bb29c935f3e30f9a86de1c241a6f6ac042c
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-01-30 21:13:44 +0100

    fixVersion1.0

--- java_lessons/src/main/java/my/example/ifSwitchLoops.java
+++ java_lessons/src/main/java/my/example/ifSwitchLoops.java
@@ -19,35 +19,41 @@ public class ifSwitchLoops {
 
 
     public static void useSwitch() {
-        for (int a = 1; a < 6; a++){
+        for (int a = 1; a < 6; a++) {
             switch (a) {
                 case 1 -> System.out.println("One");
                 case 2 -> System.out.println("Two");
                 case 3 -> System.out.println("Three");
                 case 4 -> System.out.println("Four");
                 case 5 -> System.out.println("Five");
-                }
             }
         }
+    }
 
-    public static void useWhile() {
-        int b = 10;
-        int c = 0;
-        do {
-            c += b;
-            b += 2;
-        } while (b < 55);
-        System.out.println("Сумма чётных чисел =" + c);
 
+    public static void Solution() {
+
+        int sum = 0;
+        int n = 9;
+        while (n < 55) {
+            n++;
+            if (n % 2 == 0) {
+                sum = sum + n;
+                n++;
+            }
+        }
+        System.out.println("Сумма чётных чисел = " + " " + sum);
     }
 
 
     public static void main(String[] args) {
-    ifPlusFor();
-    System.out.println(" ");
-    useSwitch();
-    System.out.println(" ");
-    useWhile();
+        ifPlusFor();
+        System.out.println(" ");
+        useSwitch();
+        System.out.println(" ");
+        Solution();
+
+
     }
 }
 
