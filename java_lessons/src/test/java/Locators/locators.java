20f943878a52f81499011a53499f3143ace7ab5d
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-02-23 01:19:51 +0100

    Дз по XPath

--- java_lessons/src/test/java/Locators/locators.java
+++ java_lessons/src/test/java/Locators/locators.java
@@ -14,6 +14,20 @@ public class locators {
     By idButtonAddCard = By.cssSelector("#add-to-cart-sauce-labs-backpack");//локатор для нажождения кнопок добавления в корзину по айди
     By allbuttonInDiv = By.cssSelector("#header_container button"); // локатор для нахождения всех кнопок внутри элемента с id = header_container
     By inventoryNameClass = By.cssSelector(".inventory_item_name"); //локатор для названий продуктов
-    By addToCardWithCss = By.cssSelector("button[data-test]"); //локатор для нахождения элементов добавить в корзину по атрибуту 
+    By addToCardWithCss = By.cssSelector("button[data-test]"); //локатор для нахождения элементов добавить в корзину по атрибуту
 
+    //А теперь поиспользуем XPath
+
+    By fullText = By.xpath("//a[text()='Twitter']");
+    By partText = By.xpath("//a[contains(text(),'Face')]");
+    By tag = By.xpath("//select");
+    By atrName = By.xpath("//button[@name='checkout']");
+    By idXpath = By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']");
+    By allXpath = By.xpath("//*");
+    By index = By.xpath("(//button[@class='btn btn_primary btn_small btn_inventory'])[2]");
+    By or = By.xpath("//div[@class='inventory_item_label' or @class='inventory_item_img']");
+    By and = By.xpath("//button[contains(@class,'btn_primary') and contains(@name,'add-to-cart-sauce-labs-onesie')]");
+    By axesButton = By.xpath("//div[@class='inventory_item_price']/following::button");
+    By axesPrice = By.xpath("//button/following::div[@class='inventory_item_price']");
+    
 }
