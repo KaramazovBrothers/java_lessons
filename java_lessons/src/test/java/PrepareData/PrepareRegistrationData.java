1120dde4ef7790b88c0200b22754aff09cc309cf
Dmitriy Korolev <59607937+KaramazovBrothers@users.noreply.github.com>
2023-03-06 19:07:03 +0100

    Добавил методов с данными со страницы

--- java_lessons/src/test/java/PrepareData/PrepareRegistrationData.java
+++ java_lessons/src/test/java/PrepareData/PrepareRegistrationData.java
@@ -14,11 +14,45 @@ public class PrepareRegistrationData {
                 .build();
     }
 
+//забираем параметры логина со страницы и логинимся со случайными валидными кредами
     public static LoginPageRegistrationModel getWithDataFromLoginPage() {
         return LoginPageRegistrationModel
                 .builder()
                 .login(LoginPage.getLogin())
                 .password(LoginPage.getPassword())
+                .build();
+    }
+
+//хардкодим параметры со страницы
+    public static LoginPageRegistrationModel getWithDataFromLoginPageOne() {
+        return LoginPageRegistrationModel
+                .builder()
+                .login("standard_user")
+                .password("secret_sauce")
+                .build();
+    }
+
+    public static LoginPageRegistrationModel getWithDataFromLoginPageTwo() {
+        return LoginPageRegistrationModel
+                .builder()
+                .login("locked_out_user")
+                .password("secret_sauce")
+                .build();
+    }
+
+    public static LoginPageRegistrationModel getWithDataFromLoginPageThree() {
+        return LoginPageRegistrationModel
+                .builder()
+                .login("problem_user")
+                .password("secret_sauce")
+                .build();
+    }
+
+    public static LoginPageRegistrationModel getWithDataFromLoginPageFour() {
+        return LoginPageRegistrationModel
+                .builder()
+                .login("performance_glitch_user")
+                .password("secret_sauce")
                 .build();
     }
 }
